bb4a948ffe099e3732eabd6fe335f62daedcd9fa
Gabriel Farrell <g@gsf.io>
2015-12-18 14:57:22 -0500

    Add joia config

---
+++ .joia.sh
@@ -0,0 +1,19 @@
+# joia config
+
+joia_install () {
+  # Set up .ssh/environment
+  joia_ssh "scripts/user-env.sh"
+
+  # Run install scripts
+  joia_ssh "
+scripts/install.sh
+scripts/gen-cert.sh
+scripts/wp-config.sh
+scripts/local-db.sh
+scripts/unison.sh
+"
+}
+
+joia_deploy () {
+  joia_ssh "scripts/deploy.sh"
+}
