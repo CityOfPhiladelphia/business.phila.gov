754a43ae76604dc846ced2c660ad61904c528a4d
Gabriel Farrell <g@gsf.io>
2015-12-18 17:15:49 -0500

    Fix db import

--- scripts/local-db.sh
+++ scripts/local-db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y mysql-server-5.6
 
@@ -8,4 +10,4 @@ wp db reset --yes
 
 echo "Loading database dump into database"
 _dir="$(dirname "$0")"
-curl -s "$("$_dir/s3url.sh" "$PHILA_DB_BUCKET" current.sql)" | wp db import -
+curl -s "$("$_dir/s3url.sh" "$DB_BUCKET" business.sql)" | wp db import -
