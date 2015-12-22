577367fe0f99eb2fa9060efce83f0d95b0aa8e42
Gabriel Farrell <g@gsf.io>
2015-12-22 17:20:35 -0500

    No activation on megaproject

--- scripts/non-free.sh
+++ scripts/non-free.sh
@@ -4,12 +4,9 @@
 
 _dir="$(dirname "$0")"
 plugins="gdlr-shortcode-1.0.zip masterslider-2.20.4.zip"
-themes="megaproject-1.07.zip"
 
 for plugin in $plugins; do
   wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PLUGIN_BUCKET" "$plugin")"
 done
 
-for theme in $themes; do
-  wp theme install --quiet --force --activate "$("$_dir/s3url.sh" "$PLUGIN_BUCKET" "$theme")"
-done
+wp theme install --quiet --force "$("$_dir/s3url.sh" "$PLUGIN_BUCKET" megaproject-1.07.zip)"
