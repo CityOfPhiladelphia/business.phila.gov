2db790b0bcf627631bfed3b9c0ed92ac6b85eb70
Gabriel Farrell <g@gsf.io>
2015-12-22 17:21:08 -0500

    Correct variable for media bucket

--- scripts/wp-config.sh
+++ scripts/wp-config.sh
@@ -31,10 +31,7 @@ define('WP_HOME', 'https://$DOMAIN');
 /** For AWS and S3 usage */
 define('AWS_ACCESS_KEY_ID', '$AWS_ID');
 define('AWS_SECRET_ACCESS_KEY', '$AWS_SECRET');
-define('AS3CF_BUCKET', '$PHILA_MEDIA_BUCKET');
-
-/** For Swiftype search */
-define('SWIFTYPE_ENGINE', '$SWIFTYPE_ENGINE');
+define('AS3CF_BUCKET', '$MEDIA_BUCKET');
 
 /** https://wordpress.org/support/topic/problem-after-the-recent-update */
 define('FS_METHOD', 'direct');
