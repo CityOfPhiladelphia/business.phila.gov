ffcf69fa16aa5f0f236c15a78a43c32192c26fd2
Karissa Demi <k@karissademi.com>
2017-09-07 11:04:13 -0400

    Add google-maps-pro

--- scripts/non-free.sh
+++ scripts/non-free.sh
@@ -3,7 +3,7 @@
 # Install non-free plugins and themes
 
 _dir="$(dirname "$0")"
-plugins="gdlr-shortcode-1.0.zip masterslider-2.20.4.zip"
+plugins="gdlr-shortcode-1.0.zip masterslider-2.20.4.zip wp-google-maps-pro-6.13.zip"
 
 for plugin in $plugins; do
   wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PLUGIN_BUCKET" "$plugin")"
